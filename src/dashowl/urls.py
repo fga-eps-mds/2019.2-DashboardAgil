b0e2dace1bda44544c5fbc8a86366df2191f4b99
Kalebe Lopes <calbkalebe@gmail.com>
2019-10-18 16:55:35 -0300

    catalog URL

--- src/dashowl/urls.py
+++ src/dashowl/urls.py
@@ -19,3 +19,20 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.conf.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/')),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
