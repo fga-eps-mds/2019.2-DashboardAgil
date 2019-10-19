cba0f0ac6a2c7cad8fd3976df667034e408301c3
Kalebe Lopes <calbkalebe@gmail.com>
2019-10-19 17:41:59 -0300

    update template django

--- src/catalog/urls.py
+++ src/catalog/urls.py
@@ -3,5 +3,5 @@ from catalog import views
 
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'charts-chartjs.html')
