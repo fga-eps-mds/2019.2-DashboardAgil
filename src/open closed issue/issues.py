c50f34be9e76754972a2f58078bdea27ff838cd6
Kalebe Lopes <calbkalebe@gmail.com>
2019-10-21 23:35:46 -0300

    open issues

---
+++ src/open closed issue/issues.py
@@ -0,0 +1,19 @@
+from github import Github
+
+#  username and password
+g = Github("KalebeLopes", "Kalebe1230,123")
+
+#   ISSUES ABERTAS 
+
+repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil-Wiki")
+open_issues = repo.get_issues(state='open')
+for issue in open_issues:
+    print(issue)
+
+
+#   ISSUES FECHADAS 
+
+repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil-Wiki")
+open_issues = repo.get_issues(state='closed')
+for issue in open_issues:
+    print(issue)
