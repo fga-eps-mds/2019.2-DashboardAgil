3854cd9718377bed8a41e397c817442f3ad9c079
KalebeLopes <calbkalebe@gmail.com>
2019-10-23 01:45:43 -0300

    add homepage and

--- src/catalog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'charts-chartjs.html')
