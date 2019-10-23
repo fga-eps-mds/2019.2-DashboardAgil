5c84c3c4e703be6e58547bf49aef86ca0aba426c
KalebeLopes <calbkalebe@gmail.com>
2019-10-22 23:59:07 -0300

    issues creator

--- src/open closed issue/issues.py
+++ src/open closed issue/issues.py
@@ -17,3 +17,11 @@ repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil-Wiki")
 open_issues = repo.get_issues(state='closed')
 for issue in open_issues:
     print(issue)
+
+
+#   ISSUES FECHADAS 
+
+repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil-Wiki")
+open_issues = repo.get_issues(creator='github.KalebeLopes.KalebeLopes')
+for issue in open_issues:
+    print(issue)
