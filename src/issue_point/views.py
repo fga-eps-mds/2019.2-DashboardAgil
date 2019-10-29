385b44632a238075b5b5c05550b87637d36065ca
muriloschiler <muriloschiler.ls@gmail.com>
2019-10-29 08:56:31 -0300

    template issue-points

--- src/issue_point/views.py
+++ src/issue_point/views.py
@@ -4,9 +4,12 @@ import json
 
 #GET /p1/repositories/:repo_id/issues/:issue_number
 
+def home(request):
 
-req= requests.get('https://api.zenhub.io/p1/repositories/206358281/issues/39?access_token=88ecf0f6ad902fa218d3eb2be63b0ff1a4b2f53b3c49aa9493279638612cf7657d22dc87e6892bc3') 
+#req= requests.get('https://api.zenhub.io/p1/repositories/206358281/issues/38?access_token=88ecf0f6ad902fa218d3eb2be63b0ff1a4b2f53b3c49aa9493279638612cf7657d22dc87e6892bc3') 
 
-pontos_issue = req.json()
+#pontos_issue = req.json()
 
-print(pontos_issue["estimate"]["value"])
+#print(pontos_issue["estimate"]["value"])
+
+    return render(request,'issue_point.html')
