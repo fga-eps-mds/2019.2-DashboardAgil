5ee6ee7b93ef6467c430b2cdcf8ee1e86d7341e5
KalebeLopes <calbkalebe@gmail.com>
2019-10-28 23:45:23 -0300

    add index

--- src/dashowl/index/urls.py
+++ src/dashowl/index/urls.py
@@ -1,4 +1,4 @@
-"""dashowl URL Configuration
+"""index URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', include('dashowl.index.urls')),
-    path('', include('dashowl.homepage.urls')),
-    path('pullrequests', include('dashowl.pull_requests.urls')),
 ]
