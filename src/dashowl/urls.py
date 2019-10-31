6f97df43c1604c6d56390e6dee25f84f441821f1
muriloschiler <muriloschiler.ls@gmail.com>
2019-10-31 12:00:32 -0300

    nothing

--- src/dashowl/urls.py
+++ src/dashowl/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path ,include
 
 urlpatterns = [
-    path('', include ('issue_point.urls') ),
+    path('issue', include ('issue_point.urls') ),
     path('admin/', admin.site.urls),
 ]
