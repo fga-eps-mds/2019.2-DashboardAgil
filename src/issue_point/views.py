08b1c7162c1773660c9aabad96be64f25121b5cc
muriloschiler <muriloschiler.ls@gmail.com>
2019-10-31 07:40:59 -0300

    prototipo do app

--- src/issue_point/views.py
+++ src/issue_point/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 import requests
 import json
 
+
 #GET /p1/repositories/:repo_id/issues/:issue_number
 
 def home(request):
 
-#req= requests.get('https://api.zenhub.io/p1/repositories/206358281/issues/38?access_token=88ecf0f6ad902fa218d3eb2be63b0ff1a4b2f53b3c49aa9493279638612cf7657d22dc87e6892bc3') 
-
+#req= requests.get('https://api.zenhub.io/p1/repositories/206358281/issues/38?access_token=563259489fc7556b5f4b5cfc0000adba14e89e60eb901883dbf6f8c04f3eaa7e7e3909eb58786850') 
 #pontos_issue = req.json()
-
 #print(pontos_issue["estimate"]["value"])
 
+
     return render(request,'issue_point.html')
+
