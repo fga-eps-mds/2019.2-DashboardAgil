b9b52d0be6e9c3a8efb79746d2412396eacc23f6
KalebeLopes <calbkalebe@gmail.com>
2019-10-31 22:10:20 -0300

    update app index

--- src/dashowl/homepage/views.py
+++ src/dashowl/homepage/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 
-def homepage(request):
-    return render(request, 'homepage/homepage.html', {})
+def home(request):
+    return render(request, 'homepage.html', {})
--- src/dashowl/urls.py
+++ src/dashowl/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', include('dashowl.index.urls')),
-    path('', include('dashowl.homepage.urls')),
+    path('homepage', include('dashowl.homepage.urls')),
     path('pullrequests', include('dashowl.pull_requests.urls')),
 ]
