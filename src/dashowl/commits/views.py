1708aa5d26e0671bcfe9026ab43219bf3f23e7f3
Matheus-AM <mam.macacod@gmail.com>
2019-10-31 22:28:51 -0300

    repository's commits are already taken

--- src/dashowl/commits/views.py
+++ src/dashowl/commits/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from github import Github
 
 # Create your views here.
+
+
 def commits(request):
+    g = Github("")
     teste = "deu certo"
     return render(request, 'commits.html', {'commit':teste})
     
