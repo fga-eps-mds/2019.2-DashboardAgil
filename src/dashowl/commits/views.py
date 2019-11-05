6ef551cdf08c3d7291e7d15762098bae4354dcfc
Matheus-AM <mam.macacod@gmail.com>
2019-11-05 10:51:45 -0300

    add changes to views GET commits, total commits and commit's author; and template to show them.

--- src/dashowl/commits/views.py
+++ src/dashowl/commits/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from github import Github
 
+# from .models import Usuario
+
 # Create your views here.
 
 
 def commits(request):
-    g = Github("")
-    teste = "deu certo"
-    return render(request, 'commits.html', {'commit':teste})
+    # token = Usuario.token
+    # g = Github(token)
+    # repos = Usuario.repos
+    # repo = g.get_repo(repos[])
+    # repo.get_commit(sha)
+    # repo.get_commits(sha=NotSet, path=NotSet, since=NotSet, until=NotSet, author=NotSet)
+
+    g = Github("joao15victor08", "j15v08o19m99")
+    repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil")
+    commits = repo.get_commits()
+    totalCommits = commits.totalCount
+    # commit = repo.get_commit(sha="7c8c4aba33040cf9865a40703900ca797bc816b4")
+    # print(commit.commit.author.date)
+
+    return render(request, 'commits.html', {'commit':commits, 'total':totalCommits})
     
