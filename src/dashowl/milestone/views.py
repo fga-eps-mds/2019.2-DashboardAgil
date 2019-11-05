94a1a763b5f24d9a8115b59aee473457e5a148ae
Damarcones <damarcones@gmail.com>
2019-11-05 18:32:50 -0300

    fixing bugs

--- src/dashowl/milestone/views.py
+++ src/dashowl/milestone/views.py
@@ -3,12 +3,13 @@ from github import Github
 
 
 
-def milestone(request):
+class milestone():  
+    def milestone(request):
 
-    g = Github("joao15victor08", "j15v08o19m99")
-    repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil")
-    milestones = repo.get_milestones()
-    totalMilestone = milestone.totalCount
+        g = Github("joao15victor08", "j15v08o19m99")
+        repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil")
+        milestone = repo.get_milestones()
+        totalMilestone = milestone.totalCount
 
 
-    return render(request, 'milestone.html', {'milestone':milestones, 'total':totalMilestone})
+        return render(request, 'milestone.html', {'milestone':milestone , 'total':totalMilestone})
