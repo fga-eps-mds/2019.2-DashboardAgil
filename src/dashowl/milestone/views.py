06117412deafe829df6b96bdbaafd8c0866847b9
Damarcones <damarcones@gmail.com>
2019-11-06 14:36:20 -0300

    GET milestone from repository

--- src/dashowl/milestone/views.py
+++ src/dashowl/milestone/views.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from github import Github
 
 def milestone(request):
 
     g = Github('joao15victor08', 'j15v08o19m99')
-    repo = g.get_repo('fga-eps-mds/2019.2-DashboardAgil')
+    repo = g.get_repo('fga-eps-mds/2019.2-DashboardAgil-Wiki')
     open_milestone = repo.get_milestones(state = 'open')
 
-    # return render(request, 'milestone.html', {'milestone':open_milestone})
+    return render(request, 'milestone.html', {'milestone':open_milestone})
