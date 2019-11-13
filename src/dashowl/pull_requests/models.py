d26faecab9e56d9f1acd10cd0f38b5ebcfcc66d7
Matheus-AM <mam.macacod@gmail.com>
2019-11-13 16:31:47 -0300

    The class Pull_requests was created

--- src/dashowl/pull_requests/models.py
+++ src/dashowl/pull_requests/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pull_request(models.Model):
+    pullRequestID = models.IntegerField()
+    openedPullRequests = models.IntegerField()
+    closedPullRequests = models.IntegerField()
+    totalPullRequests = models.IntegerField() 
+
+    def publish(self):
+        self.save()
