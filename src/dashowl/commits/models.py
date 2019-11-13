d6ca0aacd3a016959c14004c44929d4174bec2d3
Matheus-AM <mam.macacod@gmail.com>
2019-11-13 15:35:35 -0300

    The class Commit was created

--- src/dashowl/commits/models.py
+++ src/dashowl/commits/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Commit(models.Model):
+    commitID = models.IntegerField()
+    totalCommits = models.IntegerField()
+    author = models.CharField(max_length=50)
+
+    
