6cf7cda44be1293cbfa0d91b53271e4bb5534f80
KalebeLopes <calbkalebe@gmail.com>
2019-11-16 11:46:42 -0300

    make a mistake in commits

--- src/dashowl/commits/urls.py
+++ src/dashowl/commits/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
-from . import views
 
-urlpatterns = [
-    path('', views.get_commits, name='commits'),
-=======
 from . import  views
 
 urlpatterns = [
     path('', views.commits, name='commits')
->>>>>>> devel
+
 ]
