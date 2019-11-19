e0a68e1ec558ad3efb0a2099ca5f585f457a195d
Joao Victor <joao15victor08@gmail.com>
2019-11-19 10:54:31 -0300

    Get acess token and login

--- src/dashowl/index/models.py
+++ src/dashowl/index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario (models.Model):
+    login = models.CharField(max_length = 200)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
--- src/dashowl/index/views.py
+++ src/dashowl/index/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+import requests
+from github import Github
 
 def index (request):
-    return render(request, 'index.html', {})
+
+    code  = request.GET["code"]
+    r = requests.post("https://github.com/login/oauth/access_token?client_id=1b10677ef95a29f3fdf2&client_secret=9e57bce3d353b44c84f03af3101daa93a1fbb33a&code="+code)
+
+    t = r.text
+    token = ""
+    for i in range(13,53):
+        token += t[i]
+
+    g = Github(token)
+    user  = g.get_user()
+    login = user.login
+
+    
+
+    return render(request, 'index.html', {"token": })
