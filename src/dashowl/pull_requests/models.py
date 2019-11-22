1ebd7c5fb61502ba65d0c6d1482d2cf69b4298da
Matheus-AM <mam.macacod@gmail.com>
2019-11-21 23:26:17 -0300

    A rework on database, and code to save not working

--- src/dashowl/pull_requests/models.py
+++ src/dashowl/pull_requests/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Pull_request(models.Model):
     # repoID = models.ForeignKey(Repository)
-    pullRequestID = models.IntegerField()
-    openedPullRequests = models.IntegerField()
-    closedPullRequests = models.IntegerField()
-    totalPullRequests = models.IntegerField() 
+    pull_request_number = models.IntegerField()
+    state = models.CharField(max_length=20)
+    open_date = models.DateTimeField()
+    close_date = models.DateTimeField()
 
     def publish(self):
         self.save()
