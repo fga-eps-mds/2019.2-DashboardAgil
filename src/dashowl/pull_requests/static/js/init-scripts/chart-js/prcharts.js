2d13fe326cab87debe235f0792560f47e19d1111
KalebeLopes <calbkalebe@gmail.com>
2019-11-26 20:33:11 -0300

    completed PR status

--- src/dashowl/pull_requests/static/js/init-scripts/chart-js/prcharts.js
+++ src/dashowl/pull_requests/static/js/init-scripts/chart-js/prcharts.js
@@ -208,6 +208,11 @@
         }
     } ); */
 
+    var cod_1o = document.getElementById("1o").getAttribute("data-produto");
+    var cod_2o = document.getElementById("2o").getAttribute("data-produto");
+    var cod_3o = document.getElementById("3o").getAttribute("data-produto");
+    var cod_4o = document.getElementById("4o").getAttribute("data-produto");
+    var cod_5o = document.getElementById("5o").getAttribute("data-produto");
     var cod_6o = document.getElementById("6o").getAttribute("data-produto");
     var cod_7o = document.getElementById("7o").getAttribute("data-produto");
     var cod_8o = document.getElementById("8o").getAttribute("data-produto");
@@ -216,6 +221,11 @@
     var cod_11o = document.getElementById("11o").getAttribute("data-produto");
     var cod_12o = document.getElementById("12o").getAttribute("data-produto");
 
+    var cod_1c = document.getElementById("1c").getAttribute("data-produto");
+    var cod_2c = document.getElementById("2c").getAttribute("data-produto");
+    var cod_3c = document.getElementById("3c").getAttribute("data-produto");
+    var cod_4c = document.getElementById("4c").getAttribute("data-produto");
+    var cod_5c = document.getElementById("5c").getAttribute("data-produto");
     var cod_6c = document.getElementById("6c").getAttribute("data-produto");
     var cod_7c = document.getElementById("7c").getAttribute("data-produto");
     var cod_8c = document.getElementById("8c").getAttribute("data-produto");
@@ -224,7 +234,7 @@
     var cod_11c = document.getElementById("11c").getAttribute("data-produto");
     var cod_12c = document.getElementById("12c").getAttribute("data-produto");
     
-        alert("Produto passado: " + cod_10c + ", " + cod_11c + ", " + cod_12c);
+        
 
     //bar chart
     var ctx = document.getElementById( "barChart" );
@@ -236,14 +246,14 @@
             datasets: [
                 {
                     label: "Opened",
-                    data: [1, 7, 9, 5, 13, cod_6o, cod_7o, cod_8o, cod_9o, cod_10o, cod_11o, cod_12o],
+                    data: [cod_1o, cod_2o, cod_3o, cod_4o, cod_5o, cod_6o, cod_7o, cod_8o, cod_9o, cod_10o, cod_11o, cod_12o],
                     borderColor: "#ff0000",
                     borderWidth: "0",
                     backgroundColor: "#ff0000"
                             },
                 {
                     label: "Closed",
-                    data: [11, 7, 8, 5, 12, cod_6c, cod_7c, cod_8c, cod_9c, cod_10c, cod_11c, cod_12c],
+                    data: [cod_1c, cod_2c, cod_3c, cod_4c, cod_5c, cod_6c, cod_7c, cod_8c, cod_9c, cod_10c, cod_11c, cod_12c],
                     borderColor: '#434343',
                     borderWidth: "0",
                     backgroundColor: "#434343"
--- src/dashowl/pull_requests/views.py
+++ src/dashowl/pull_requests/views.py
@@ -16,21 +16,21 @@ def get_PullRuequests (request):
 
     if bool(Pull_request.objects.filter(repository__repositoryID=repo.id)):
         pull_requests = Pull_request.objects.filter(repository__repositoryID=repo.id)
-        pulls_open = Pull_request.objects.filter(repository__repositoryID=repo.id, state='open')
-        pulls_closed = Pull_request.objects.filter(repository__repositoryID=repo.id, state='closed')
+        #pulls_open = Pull_request.objects.filter(repository__repositoryID=repo.id, state='open')
+        #pulls_closed = Pull_request.objects.filter(repository__repositoryID=repo.id, state='closed')
 
-        date1o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=1, state='open')
-        date2o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=2, state='open')
-        date3o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=3, state='open')
-        date4o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=4, state='open')
-        date5o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=5, state='open')
-        date6o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=6, state='open')
-        date7o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=7, state='open')
-        date8o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=8, state='open')
-        date9o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=9, state='open')
-        date10o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=10, state='open')
-        date11o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=11, state='open')
-        date12o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=12, state='open')
+        date1o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=1)
+        date2o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=2)
+        date3o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=3)
+        date4o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=4)
+        date5o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=5)
+        date6o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=6)
+        date7o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=7)
+        date8o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=8)
+        date9o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=9)
+        date10o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=10)
+        date11o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=11)
+        date12o = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=12)
 
         date1c = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=1, state='closed')
         date2c = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=2, state='closed')
@@ -50,7 +50,7 @@ def get_PullRuequests (request):
         raise TypeError
 
     
-    return render(request, 'pull_requests.html', {'date1o': date1o, 'date2o': date2o,'date3o': date3o,'date4o': date4o,'date5o': date5o,'date6o': date6o,
+    return render(request, 'pull_requests.html', {'pull_requests': pull_requests, 'date1o': date1o, 'date2o': date2o,'date3o': date3o,'date4o': date4o,'date5o': date5o,'date6o': date6o,
     'date7o': date7o,'date8o': date8o,'date9o': date9o,'date10o': date10o,'date11o': date11o,'date12o': date12o,'date1c': date1c, 'date2c': date2c,
     'date3c': date3c,'date4c': date4c,'date5c': date5c,'date6c': date6c, 'date7c': date7c,'date8c': date8c,'date9c': date9c,'date10c': date10c,
     'date11c': date11c,'date12c': date12c})
