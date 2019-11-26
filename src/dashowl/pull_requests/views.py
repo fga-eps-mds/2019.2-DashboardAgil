c90ef57a6525a00301c9cf52a743bb46362b5546
KalebeLopes <calbkalebe@gmail.com>
2019-11-26 13:36:44 -0300

    data in html

--- src/dashowl/pull_requests/views.py
+++ src/dashowl/pull_requests/views.py
@@ -14,24 +14,12 @@ def get_PullRuequests (request):
     repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil")
 
 
-    pulls_open = repo.get_pulls(state="open")
-    pulls_closed = repo.get_pulls(state="closed")
-
-    total_closed = pulls_closed.totalCount
-    total_open = pulls_open.totalCount
-
-    total = pulls_open.totalCount
-    total += pulls_closed.totalCount
-
-
     if bool(Pull_request.objects.filter(repository__repositoryID=repo.id)):
         pull_requests = Pull_request.objects.filter(repository__repositoryID=repo.id)
         pulls_open = Pull_request.objects.filter(repository__repositoryID=repo.id, state='open')
         pulls_closed = Pull_request.objects.filter(repository__repositoryID=repo.id, state='closed')
     else:
         raise TypeError
-
-
 
 
     return render(request, 'pull_requests.html', {'pull_requests': pull_requests, 'pulls_open': pulls_open, 'pulls_closed': pulls_closed})
