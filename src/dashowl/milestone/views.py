91909d27d1d5ade7d8224d459888e585cb9d3cc4
Damarcones <damarcones@gmail.com>
2019-11-26 16:00:03 -0300

    fixing conflict

--- src/dashowl/milestone/views.py
+++ src/dashowl/milestone/views.py
@@ -10,12 +10,12 @@ def get_milestone(request):
     repo = g.get_repo('fga-eps-mds/2019.2-DashboardAgil-Wiki')
     open_milestone = repo.get_milestones(state='all')
 
-        if bool(Milestone.objects.filter(repository__repositoryID=repo.id)):
-            all_milestone = Milestone.objects.filter(repository__repositoryID=repo.id)
-            open_milestone = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
-            closed_milestone = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
-        else:
-            raise TypeError
+    if bool(Milestone.objects.filter(repository__repositoryID=repo.id)):
+        all_milestone = Milestone.objects.filter(repository__repositoryID=repo.id)
+        open_milestone = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
+        closed_milestone = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+    else:
+        raise TypeError
 
 #Retorna o valor de cada issue
     # req= requests.get('https://api.zenhub.io/p1/repositories/206358281/issues/39?access_token=02a009e06e4926091eadce6ef1dffc9f9b3f7b5bd417b116ea90c55bf6fb68dda7eb367ab6544c07')
