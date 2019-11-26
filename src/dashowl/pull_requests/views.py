4efceac9f87763dfaf9bf8958098c95322157ac1
KalebeLopes <calbkalebe@gmail.com>
2019-11-26 14:12:54 -0300

    return count per month

--- src/dashowl/pull_requests/views.py
+++ src/dashowl/pull_requests/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from github import Github
 from .models import Pull_request
 from .. import secret
+import datetime
 
 
 def get_PullRuequests (request):
@@ -13,14 +14,15 @@ def get_PullRuequests (request):
     g = Github(secret.login, secret.password)
     repo = g.get_repo("fga-eps-mds/2019.2-DashboardAgil")
 
-
     if bool(Pull_request.objects.filter(repository__repositoryID=repo.id)):
         pull_requests = Pull_request.objects.filter(repository__repositoryID=repo.id)
         pulls_open = Pull_request.objects.filter(repository__repositoryID=repo.id, state='open')
         pulls_closed = Pull_request.objects.filter(repository__repositoryID=repo.id, state='closed')
+        date = Pull_request.objects.filter(repository__repositoryID=repo.id, open_date__month=11)
     else:
         raise TypeError
 
+    
 
-    return render(request, 'pull_requests.html', {'pull_requests': pull_requests, 'pulls_open': pulls_open, 'pulls_closed': pulls_closed})
+    return render(request, 'pull_requests.html', {'date': date, 'pulls_open': pulls_open, 'pulls_closed': pulls_closed})
 
