177675051abd870e81b99426b8889db92dfd8389
Joao Victor <joao15victor08@gmail.com>
2019-11-27 09:50:38 -0300

    Remove debug in index view

--- src/dashowl/index/views.py
+++ src/dashowl/index/views.py
@@ -39,13 +39,13 @@ def index (request):
                 
                 else:
                     token_nd = 'nao e digito'
-                    return render(request, 'index_s_token.html', {"token": token_nd})
+                    return render(request, 'index_s_token.html', {})
             else:
                 code_nd = 'code nao existe'
-                return render(request, 'index_s_token.html', {"token": code_nd})
+                return render(request, 'index_s_token.html', {})
         else:
             sem_code = 'sem code'
-            return render(request, 'index_s_token.html', {"token": sem_code})
+            return render(request, 'index_s_token.html', {})
     else:
         # session_set = 'session seteda'
         return render(request, 'index.html', {})
