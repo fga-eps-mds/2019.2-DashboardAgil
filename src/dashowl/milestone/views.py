18c2147246e79a49aec2f3ddef835e6ee19b2b9e
Joao Victor <joao15victor08@gmail.com>
2019-11-28 08:31:41 -0300

    Add milestones verifications

--- src/dashowl/milestone/views.py
+++ src/dashowl/milestone/views.py
@@ -7,35 +7,68 @@ from ..repositories.models import Repository
 
 def get_milestone(request):
     
-    user_login = request.session['login']
-    token = request.session['token']
+    if 'id' not in request.session:
+        if request.method == 'GET' and 'id' in request.GET:
 
-    repo_id = request.GET["id"]
+            user_login = request.session['login']
+            token = request.session['token']
 
-    request.session['id'] = repo_id
+            repo_id = request.GET["id"]
 
-    token = request.session['token']
+            request.session['id'] = repo_id
 
-    g = Github(token)
-    
-    repo = g.get_repo(full_name_or_id=int(repo_id))
+            token = request.session['token']
 
-    repository = Repository.objects.get(repositoryID=repo.id)
-    if bool(Milestone.objects.filter(repository__repositoryID=repo.id)):
-        milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
-        refresh_milestones(repo, list(milestones)[-1].repository, list(milestones)[-1].milestone_number)
-        milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
-        milestones_open = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
-        milestones_closed = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+            g = Github(token)
+            
+            repo = g.get_repo(full_name_or_id=int(repo_id))
 
+            repository = Repository.objects.get(repositoryID=repo.id)
+            if bool(Milestone.objects.filter(repository__repositoryID=repo.id)):
+                milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
+                refresh_milestones(repo, list(milestones)[-1].repository, list(milestones)[-1].milestone_number)
+                milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
+                milestones_open = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
+                milestones_closed = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+
+            else:
+                save_milestone(repo, repository)
+                milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
+                milestones_open = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
+                milestones_closed = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+
+            return render(request, 'milestone.html', {'milestones': milestones, 'milestones_open': milestones_open, 'milestones_closed': milestones_closed})
+        else:
+            sem_id = "Selecione um repositório"
+            return render(request, 'milestone.html', {'id':sem_id})
     else:
-        save_milestone(repo, repository)
-        milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
-        milestones_open = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
-        milestones_closed = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+        user_login = request.session['login']
+        token = request.session['token']
 
-    return render(request, 'milestone.html', {'milestones': milestones, 'milestones_open': milestones_open, 'milestones_closed': milestones_closed})
+        repo_id = request.session['id']
 
+        token = request.session['token']
+
+        g = Github(token)
+        
+        repo = g.get_repo(full_name_or_id=int(repo_id))
+
+        repository = Repository.objects.filter(repositoryID=repo.id)
+        if bool(Milestone.objects.filter(repository__repositoryID=repo.id)):
+            milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
+            refresh_milestones(repo, list(milestones)[-1].repository, list(milestones)[-1].milestone_number)
+            milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
+            milestones_open = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
+            milestones_closed = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+
+        else:
+            save_milestone(repo, repository)
+            milestones = Milestone.objects.filter(repository__repositoryID=repo.id).order_by('milestone_number')
+            milestones_open = Milestone.objects.filter(repository__repositoryID=repo.id, state='open')
+            milestones_closed = Milestone.objects.filter(repository__repositoryID=repo.id, state='closed')
+
+        return render(request, 'milestone.html', {'milestones': milestones, 'milestones_open': milestones_open, 'milestones_closed': milestones_closed})
+              
 
 def refresh_milestones(repo, repository, last):
     milestones = repo.get_milestones(state='all')
